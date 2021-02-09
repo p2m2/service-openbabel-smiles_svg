f1385d29a4912c13d2936ddec353c633e6433151
Olivier Filangi <olivier.filangi@gmail.com>
2021-02-09 11:51:37 +0100

    web service to request smiles svg

--- README.md
+++ README.md
@@ -1,9 +1,33 @@
 # service-openbabel-smiles_svg
 service to convert a smiles code to svg
 
-## use
+## use with bash command
 
 ```bash
-docker run -t p2m2/service-openbabel-smiles_svg smiles_to_svg "[H][C@]1(CO)O[C@@]([H])(O[C@@]2([H])[C@]([H])(O)[C@@]([H])(COC(=O)CC(O)=O)O[C@@]([H])(OC3=CC4=C(O)C=C(O)C=C4[O+]=C3C3=CC=C(O)C(O)=C3)[C@]2([H])O)[C@]([H])(O)[C@@]([H])(O)[C@]1([H])O" > smiles_mol.svg
+docker run -t inraep2m2/service-openbabel-smiles_svg smiles_to_svg "CN1CCC[C@H]1c2cccnc2" > smiles_mol.svg
 ```
 
+## web service
+
+return a svg image with the following request `http://<host>/smiles?<smiles>`
+
+### use in img tag  
+
+```html
+<img src="http://localhost:3000/smiles?CN1CCC[C@H]1c2cccnc2" decoding="async" loading="lazy" alt="Nicotine" >
+```
+
+
+### env var
+
+- PORT   : web service port  (*default*:`3000`) 
+- MAX_RETENTION : number of svg "in memory" build by open-babel (*default*:`10 000`) 
+- SIZE_CLEAN_RETENTION : number of clean group when max retention is reached (*default*:`500`)
+
+retention policy : oldest ask are removed.
+
+
+```bash
+docker run -p 3000:30005 --env PORT=30005 --env MAX_RETENTION=50000 --env SIZE_CLEAN_RETENTION=10 \ 
+-t inraep2m2/service-openbabel-smiles_svg smiles_to_svg "CN1CCC[C@H]1c2cccnc2" > smiles_mol.svg
+```
---
+++ service.js
@@ -0,0 +1,83 @@
+const os = require("os");
+const express = require('express') ;
+const { exec } = require('child_process');
+
+const app = express() ;
+var port = 3000 ;
+var max_retention = 10000 ;
+var size_clean_retention = 500 ;
+
+if (process.env.PORT) {
+    port = process.env.PORT
+}
+
+if (process.env.MAX_RETENTION) {
+    max_retention = process.env.MAX_RETENTION
+}
+
+if (process.env.SIZE_CLEAN_RETENTION) {
+    size_clean_retention = process.env.SIZE_CLEAN_RETENTION
+}
+
+
+/* security */
+if (size_clean_retention>=max_retention) { size_clean_retention=5 ; }
+
+var retention = [] ;
+
+app.get('/smiles', (req, res) => {
+
+  let smiles_code=req.url.split("?")[1] ;
+  console.log(smiles_code)
+
+  if (!smiles_code || smiles_code.trim()=="") {
+     console.error("smiles is not defined.") ;
+     res.send("") ;
+     return ;
+  }
+
+  res.setHeader('content-type', 'image/svg+xml');
+  let idxObj = null ;
+  if ( idxObj = retention
+               .map( (v,i) => { return { 'v' : v, 'i':i} } )
+               .filter( o => o.v.smiles == smiles_code )[0] ) {
+    console.log(" - memory - ")
+    retention[idxObj.i].date = Date.now();
+    res.send(idxObj.v.svg);
+  } else {
+     let cmd=`obabel -:"${smiles_code}" -o svg | grep -v "molecule converted"`
+     console.log(cmd)
+     exec(cmd, (err, stdout, stderr) => {
+      if (err) {
+        //some err occurred
+        console.error(err)
+      } else {
+        //let svg = stdout.replace(/[\n\r]/g, '').trim() ;
+
+        console.log(Object.keys(retention).length)
+        if ( Object.keys(retention).length >= max_retention ) {
+            retention = retention.sort( (v1,v2) => v1.date>v2.date).splice(retention.length - size_clean_retention);
+        }
+        /* set current retention */
+        svg = stdout.trim()
+        retention.push({
+                'smiles' : smiles_code,
+                'date' : Date.now(),
+                'svg' : svg
+        })
+        console.error(stderr);
+        res.send(svg)
+      }
+    });
+
+  }
+
+
+
+})
+
+app.listen(port, () => {
+  console.log(`MAX_RETENTION:${max_retention}`)
+  console.log(`SIZE_CLEAN_RETENTION:${size_clean_retention}`)
+  console.log(`SVG Smiles (Open-Babel)) app listening at http://${os.hostname()}:${port}`)
+})
